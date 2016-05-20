a31427f9f375d61c7271979aec984da94b404eb6
Andy <andy@workshopshed.com>
2016-05-20 09:04:07 +0100

    First Dragon Classifier

---
+++ TestOpenCVDragonClassifier.py
@@ -0,0 +1,27 @@
+# A simple test file for OpenCV
+
+import numpy as np
+import cv2
+import datetime
+
+def detect(img_color):
+    dclassifier = cv2.CascadeClassifier('DragonClassifier50x50-5.xml')
+
+    detected = 0
+    gray = cv2.cvtColor(img_color, cv2.COLOR_BGR2GRAY)
+
+    # Now we find the faces in the image. If faces are found, it returns the positions of detected faces as Rect(x,y,w,h).
+    # Once we get these locations, we can create a ROI for the face and apply eye detection on this ROI (since eyes are always on the face !!! ).
+    dragons = dclassifier.detectMultiScale(gray)
+    for (x, y, w, h) in dragons:
+        cv2.rectangle(img_color, (x, y), (x + w, y + h), (255, 0, 0), 2)
+        detected += 1
+
+    print(str(detected) + " dragons")
+    return detected
+
+img = cv2.imread('TestDragons.jpg')
+
+if detect(img) > 0:
+    newFileName = "TestOutput" + datetime.datetime.now().isoformat().replace(":","") + ".jpg"
+    cv2.imwrite(newFileName, img);
