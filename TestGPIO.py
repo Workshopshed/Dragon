a8d6a2967514603e9c779ef7f9406eff6278e8f1
Andy <andy@workshopshed.com>
2016-05-22 00:09:59 +0100

    Simplified examples

--- TestGPIO.py
+++ TestGPIO.py
@@ -1,20 +1,12 @@
-import time
-
 from libsoc_zero.GPIO import LED
 from time import sleep
 
 gpio_red = LED('GPIO-B')
-gpio_green = LED('GPIO-C')
-gpio_blue = LED('GPIO-D')
 
 while (True): 
     gpio_red.on()
-    gpio_green.on()	
-    gpio_blue.on()	
     sleep(0.5)
     gpio_red.off()
-    gpio_green.off()	
-    gpio_blue.off()	
     sleep(0.5)
 
     
--- TestGPIOIn.py
+++ TestGPIOIn.py
@@ -5,23 +5,15 @@ from time import sleep
 sensor = Button('GPIO-A')
 
 gpio_red = LED('GPIO-B')
-gpio_green = LED('GPIO-C')
-gpio_blue = LED('GPIO-D')
 
 gpio_red.off()
-gpio_green.off()	
-gpio_blue.off()	
 sleep(2)
 
 while True:
     if sensor.is_pressed():
         gpio_red.on()
-        gpio_green.on()	
-        gpio_blue.on()	
         sleep(0.5)
     else:
         gpio_red.off()
-        gpio_green.off()	
-        gpio_blue.off()	
         sleep(0.5)
 
